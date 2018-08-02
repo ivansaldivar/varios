6f619eeaff7e4ac3b1a02ad9bfa9c920136329c8
Ivan Saldívar Rodriguez <ivansaldivar@gmail.com>
2018-08-02 01:17:59 -0400

    Update add2.js

--- add2.js
+++ add2.js
@@ -9,5 +9,25 @@ function add2(x) {
         }
     };
 }
+
+function add3(n) {
+  sum = n;
+  const proxy = new Proxy(function a () {}, {
+    get (obj, key) {
+      return () => sum;
+    },
+    apply (receiver, ...args) {
+      sum += args[1][0];
+      return proxy;
+    },
+  });
+  return proxy
+}
+
+//Necesita () al final
 console.log( add2(2)(5)(7)() );
 console.log( add2(2)(5)() );
+
+//Sin () al final
+console.log( add3(2)(5)(7) );
+console.log( add3(2)(5) );
